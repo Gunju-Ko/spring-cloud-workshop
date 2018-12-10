072e792758ab037ae31b2bf01b9129f7b7ae1280
gunjuKo <gunju@sk.com>
2018-12-10 17:57:43 +0900

    Enable eureka client for product, display

--- display/src/main/java/com/elevenst/DisplayApplication.java
+++ display/src/main/java/com/elevenst/DisplayApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableEurekaClient
 public class DisplayApplication {
 
     @Bean
--- product/src/main/java/com/elevenst/ProductApplication.java
+++ product/src/main/java/com/elevenst/ProductApplication.java
@@ -2,8 +2,10 @@ package com.elevenst;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class);
