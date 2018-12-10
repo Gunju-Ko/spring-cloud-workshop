3bf73a8397f3ee158428217420adee488e127f33
gunjuKo <gunju@sk.com>
2018-12-10 22:56:47 +0900

    Remove url from FeignClient to enable eureka/ribbon

--- display/src/main/java/com/elevenst/service/FeignProductRemoteService.java
+++ display/src/main/java/com/elevenst/service/FeignProductRemoteService.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "product", url = "http://localhost:8082/")
+@FeignClient(name = "product")
 public interface FeignProductRemoteService {
     @RequestMapping(path = "/products/{productId}")
     String getProductInfo(@PathVariable("productId") String productId);
