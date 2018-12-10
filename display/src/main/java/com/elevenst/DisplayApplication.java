2d521227c9758694cf929ccc00fa9d701376fb56
gunjuKo <gunju@sk.com>
2018-12-10 15:49:24 +0900

    Intial Commit

---
+++ display/src/main/java/com/elevenst/DisplayApplication.java
@@ -0,0 +1,12 @@
+package com.elevenst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisplayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DisplayApplication.class);
+    }
+
+}
