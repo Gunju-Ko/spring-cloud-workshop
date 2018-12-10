71a65cbafb60b241ee03cb6b289e836bed196aad
gunjuKo <gunju@sk.com>
2018-12-10 16:53:30 +0900

    Example for circuit-open

--- product/src/main/java/com/elevenst/api/ProductController.java
+++ product/src/main/java/com/elevenst/api/ProductController.java
@@ -11,13 +11,13 @@ public class ProductController {
 
     @GetMapping(path = "{productId}")
     public String getProductInfo(@PathVariable String productId) {
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        return "[product id = " + productId + " at " + System.currentTimeMillis() + "]";
-        //throw new RuntimeException("I/O Exception");
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return "[product id = " + productId + " at " + System.currentTimeMillis() + "]";
+        throw new RuntimeException("I/O Exception");
     }
 }
