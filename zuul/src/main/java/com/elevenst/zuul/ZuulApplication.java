8af4772ec377cbaeca75ea19a16e9d64980e80d4
gunjuKo <gunju@sk.com>
2018-12-11 14:42:42 +0900

    Enable Zuul Proxy with eureka client

--- zuul/src/main/java/com/elevenst/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/elevenst/zuul/ZuulApplication.java
@@ -2,7 +2,11 @@ package com.elevenst.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ZuulApplication {
 
