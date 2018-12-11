b8788f2d6697a336235bdd0239f2640ef78ceb53
gunjuKo <gunju@sk.com>
2018-12-11 13:41:14 +0900

    add zuul module

---
+++ zuul/src/main/java/com/elevenst/zuul/ZuulApplication.java
@@ -0,0 +1,12 @@
+package com.elevenst.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
